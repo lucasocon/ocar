2cd18c5ce3200a6d2b13288fbad74a195796ee7c
Lucas Ocon <lucas_masterclas@hotmail.com>
2016-03-29 19:31:05 -0300

    New approach with typhoeus

--- lib/ocar/client.rb
+++ lib/ocar/client.rb
@@ -1,65 +1,52 @@
-require 'uri'
-require 'net/http'
 require 'json'
+require 'typhoeus'
 
 module Ocar
 
-  class UnprocessableError < RuntimeError; end
-  class ForbiddenError < RuntimeError; end
-  class UnknownResponse < RuntimeError; end
-
   extend self
 
+  TYPES = ["cartas", "paquetes", "dni", "partidas" ]
+  $hydra = Typhoeus::Hydra.new
 
-  def get_packages(track_id)
-    uri = set_params(track_id)
-    response = send_request(uri)
-    parse_json(response)
+  def get_package track_id
+    requests = setup_request track_id
+    run_request
+    get_response requests
   end
 
   private
-  def parse_json(response)
-    case response
-    when Net::HTTPSuccess
-      check_response(response)
-    when Net::HTTPUnprocessableEntity
-      raise UnprocessableError, 'Bad URI param!'
-    else
-      raise UnknownResponse, 'Something was wrong!'
-    end
+  def setup_request track_id
+    requests = TYPES.map { |type|
+      request = Typhoeus::Request.new("http://www.oca.com.ar",
+        method: :get,
+        params: { q: "package-locator",
+        type: type,number: track_id },
+        headers: { Accept: "text/html" }
+      )
+      $hydra.queue(request)
+      request
+    }
   end
 
-  def check_response(response)
-    json = JSON.parse(response.body)
-    if json['success'] == true
-      Ocar::Status.new(json)
+  def run_request
+    $hydra.run
+  end
+
+  def get_response requests
+    results = []
+    responses = requests.map do |request|
+      parsed = JSON.parse request.response.body
+      results << parsed if parsed['success'] == true
+    end
+    return check_response results
+  end
+
+
+  def check_response results
+    if results.any?
+      Ocar::Status.new(results.first)
     else
       nil
     end
-  end
-
-  def send_request(uri)
-    req = Net::HTTP::Get.new(uri.request_uri)
-    setup_headers(req)
-    http_params = [uri.hostname, uri.port, use_ssl: uri.scheme == 'https']
-    Net::HTTP.start(*http_params) {|http| http.request(req)}
-  end
-
-  def set_params(track_id)
-    uri = URI.parse("http://www.oca.com.ar")
-    uri.query = URI.encode_www_form({q: "package-locator", type: "dni", number: track_id})
-    uri
-  end
-
-  def setup_headers(req)
-    req.add_field "User-Agent","Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
-    req.add_field "Accept","application/json, text/javascript, */*; q=0.01"
-    req.add_field "Accept-Encoding", "deflate"
-    req.add_field "Accept-Language", "en-US,en;q=0.8,es;q=0.6"
-    req.add_field "Connection", "keep-alive"
-    req.add_field "Host", "www.oca.com.ar"
-    req.add_field "Referer","http://www.oca.com.ar/"
-    req.add_field "X-Requested-With","XMLHttpRequest"
-    req
   end
 end
